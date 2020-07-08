8496d0ed702a32d51e45a85ea3f267283bbc66c3
Xerwts <66620543+Xerwts@users.noreply.github.com>
2020-07-08 12:21:05 +0200

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -18,7 +18,7 @@ from userbot.system import dev_cmd
 @bot.on(dev_cmd("bye", outgoing=True))
 async def bye(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit('**Esco dalla chat!**')
+        await e.edit('**Esco da sta chat demmerda !**')
         time.sleep(3)
         if '-' in str(e.chat_id):
             await bot(LeaveChannelRequest(e.chat_id))
